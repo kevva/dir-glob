c3c47936cecc6efc69bc2cc1ab13da2cfff411c3
JM Versteeg <janmerijnversteeg@gmail.com>
2016-01-08 17:16:36 +0100

    Add support for negating globs

--- test.js
+++ test.js
@@ -11,6 +11,8 @@ test('convert directories to glob - async', async t => {
 	t.same(await m(['index.js', 'tmp'], {ext: 'js'}), ['index.js', 'tmp/**/*.js']);
 	t.same(await m(['index.js', 'tmp'], {ext: ['js', 'png']}), ['index.js', 'tmp/**/*.{js,png}']);
 	t.same(await m(['foo/**', 'tmp']), ['foo/**', 'tmp/**']);
+	t.same(await m(['index.js', '!tmp']), ['index.js', '!tmp/**']);
+	t.same(await m(['index.js', '!tmp'], {ext: ['js', 'png']}), ['index.js', '!tmp/**/*.{js,png}']);
 });
 
 test('convert directories to glob - sync', t => {
@@ -18,4 +20,6 @@ test('convert directories to glob - sync', t => {
 	t.same(m.sync(['index.js', 'tmp'], {ext: 'js'}), ['index.js', 'tmp/**/*.js']);
 	t.same(m.sync(['index.js', 'tmp'], {ext: ['js', 'png']}), ['index.js', 'tmp/**/*.{js,png}']);
 	t.same(m.sync(['foo/**', 'tmp']), ['foo/**', 'tmp/**']);
+	t.same(m.sync(['index.js', '!tmp']), ['index.js', '!tmp/**']);
+	t.same(m.sync(['index.js', '!tmp'], {ext: ['js', 'png']}), ['index.js', '!tmp/**/*.{js,png}']);
 });
